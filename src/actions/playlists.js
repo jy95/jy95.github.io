c4c571c1e3c88084d3d1127d29bf5c0a55414e11
jy95 <jacques.yakoub@gmail.com>
2019-10-19 23:40:26 +0200

    Use FlexBox for Viewer

--- src/actions/playlists.js
+++ src/actions/playlists.js
@@ -1,14 +1,14 @@
 const { YouTube } = require('better-youtube-api');
 const youtube = new YouTube("apiKey");
 // l'id de ma chaine
-const CHANNEL_ID ="UCG0N7IV-C43AM9psxslejCQ";
+const CHANNEL_ID = "UCG0N7IV-C43AM9psxslejCQ";
 
 export const FETCHING_REQUESTED = "FETCHING_REQUESTED";
 export const FETCHING_OK = "FETCHING_OK";
 export const FETCHING_FAILED = "FETCHING_FAILED";
 
 // param à la place du () du genre ({title, password})
-export const get_playlists = ()  => {
+export const get_playlists = () => {
     return (dispatch, getState) => {
 
         // pour plus tard
@@ -23,20 +23,19 @@ export const get_playlists = ()  => {
             }).catch( err => {
                 dispatch(fetchingFailed(err));
             })
-
     };
 };
 
 const fetchingStarted = () => ({
-  type: FETCHING_REQUESTED
+    type: FETCHING_REQUESTED
 });
 
 const fetchingFinished = (playlists) => ({
-   type: FETCHING_OK,
-   playlists
+    type: FETCHING_OK,
+    playlists
 });
 
 const fetchingFailed = (error) => ({
-   type: FETCHING_FAILED,
-   error
+    type: FETCHING_FAILED,
+    error
 });
--- src/components/PlaylistsViewer/CardsBox.js
+++ src/components/PlaylistsViewer/CardsBox.js
@@ -8,13 +8,23 @@ class CardsBox extends React.Component {
 
         const {data} = this.props;
 
+        // trier en fonction de la longueur du titre pour mettre un maximum d'éléments par row
+        // en attendant de trouver un meilleur fix
+        let sorted = data.sort(
+            function(a, b){
+                // ASC  -> a.length - b.length
+                // DESC -> b.length - a.length
+                return a.title.length - b.title.length
+            }
+        );
+
         return (
             <Box
                 flexWrap="wrap"
                 display="flex"
             >
                 {
-                    data.map(
+                    sorted.map(
                         playlist => <Box>
                             <CardEntry playlist={playlist}/>
                         </Box>
