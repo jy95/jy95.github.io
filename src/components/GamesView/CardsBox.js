3e8c44e4ded9afa4e65c5279f14ae60b6fbc5917
jy95 <jacques.yakoub@gmail.com>
2020-11-01 19:34:58 +0100

    WIP Sort on games gallery

--- src/components/GamesView/CardsBox.js
+++ src/components/GamesView/CardsBox.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import ImportExportIcon from '@material-ui/icons/ImportExport';
+import Typography from '@material-ui/core/Typography';
 import Box from "@material-ui/core/Box";
+
 import CardEntry from "./CardEntry";
 
 // Inspired by https://stackoverflow.com/a/60068169/6149867
-function makeMultiCriteriaSort(...criteria) {
+function makeMultiCriteriaSort(criteria) {
     return (a, b) => {
         for(let i = 0; i < criteria.length; i++) {
             const comparatorResult = criteria[i](a, b);
@@ -27,28 +32,101 @@ const sortByReleaseDateASC = (a, b) => {
 const sortByReleaseDateDESC = (a, b) => -sortByReleaseDateASC(a, b);
 
 class CardsBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSortChange = this.handleSortChange.bind(this);
+        this.state = {
+            currentSorters: [sortByNameASC], // the current sort(s) applied
+            sortersState: {
+                "name": "ASC",
+                "releaseDate": "ASC"
+            },
+            sortersKeys: ["name", "releaseDate"], // useful to recreate currentSorters
+            // in order to prevent if / else chain in the code
+            sorters: {
+                "name": {
+                    "ASC": sortByNameASC,
+                    "DESC": sortByNameDESC
+                },
+                "releaseDate": {
+                    "ASC": sortByReleaseDateASC,
+                    "DESC": sortByReleaseDateDESC
+                }
+            }
+        }
+    }
+
+    handleSortChange(field) {
+        // Invert previous state value for this field
+        const oldValue = this.state.sortersState[field];
+        const newValue = (oldValue === "ASC") ? "DESC" : "ASC";
+
+        // keep track of the sorters state
+        const updatedSortersState = Object.assign({}, this.state.sortersState, {
+            [field]: newValue
+        })
+
+        // Decide the sort algorithm now
+        // Changed field should be the first criteria, other should be unchanged (following my simple order, from now)
+        let updatedCurrentSorters = [field]
+            .concat(
+                this.state.sortersKeys.filter(s => s !== field)
+            )
+            .map(criteria => {
+                const sortFcts = this.state.sorters[criteria];
+                const state = updatedSortersState[criteria];
+                return sortFcts[state];
+            });
+        
+        // update state
+        this.setState({
+            ...this.state,
+            sortersState: updatedSortersState,
+            currentSorters: updatedCurrentSorters
+        })
+    }
+
     render() {
 
         const {data} = this.props;
 
-        // trier en fonction de la longueur du titre pour mettre un maximum d'éléments par row
-        // en attendant de trouver un meilleur fix
+        // Apply given sort choice
         let sorted = data.sort(
-            makeMultiCriteriaSort(sortByNameASC)
+            makeMultiCriteriaSort(this.state.currentSorters)
         );
 
         return (
-            <Box
-                display="flex"
-                flexWrap="wrap"
-                flexDirection="row"
-            >
-                {
-                    sorted.map(
-                        game => <CardEntry game={game} key={game.playlistId}/>
-                    )
-                }
-            </Box>
+            <div>
+                <ButtonGroup color="primary" aria-label="outlined primary button group">
+                    {
+                        this.state.sortersKeys
+                            .map(criteria => 
+                                <Button onClick={() => this.handleSortChange(criteria)}>
+                                    <ImportExportIcon />
+                                    <Typography>
+                                        { criteria === "name" &&
+                                            "Trier par nom" 
+                                        }
+                                        { criteria === "releaseDate" &&
+                                            "Trier par date de sortie"
+                                        }
+                                    </Typography>
+                                </Button>
+                            )
+                    }
+                </ButtonGroup>
+                <Box
+                    display="flex"
+                    flexWrap="wrap"
+                    flexDirection="row"
+                >
+                    {
+                        sorted.map(
+                            game => <CardEntry game={game} key={game.playlistId}/>
+                        )
+                    }
+                </Box>
+            </div>
         )
     }
 }
