d0c832c5a1f439e9956b98c720c74dc1ed8bdeef
jy95 <jacques.yakoub@gmail.com>
2020-11-03 20:48:51 +0100

    Updates

---
+++ precache-manifest.504e0a156b33237e5ce4b2150547e51b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27cc3598efc7f08952e358b0dd91ddcf",
+    "url": "/yt_gaming_library/index.html"
+  },
+  {
+    "revision": "24626b3a90497929c55c",
+    "url": "/yt_gaming_library/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "0f4ad6ebdccfa758d084",
+    "url": "/yt_gaming_library/static/js/2.b6bf81d2.chunk.js"
+  },
+  {
+    "revision": "6472a7d1d95598048b2f7b02095da167",
+    "url": "/yt_gaming_library/static/js/2.b6bf81d2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24626b3a90497929c55c",
+    "url": "/yt_gaming_library/static/js/main.309c5ee1.chunk.js"
+  },
+  {
+    "revision": "3f699ebdc7a0d9cdcc12",
+    "url": "/yt_gaming_library/static/js/runtime-main.3b75c539.js"
+  }
+]);
