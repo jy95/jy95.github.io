0d6f6d5b0f2588dbb7f45af1fbc1164c319812c7
jy95 <jacques.yakoub@gmail.com>
2020-11-08 12:08:14 +0100

    WIP Refactor Sorters

--- src/components/GamesView/GamesSorters.js
+++ src/components/GamesView/GamesSorters.js
@@ -17,97 +17,90 @@ import {sort_games} from "../../actions/games";
 
 
 // Sort buttons of GamesGallery
-class GamesSorters extends React.Component {
+function GamesSorters(props) {
     
-    constructor(props) {
-        super(props);
-        this.handleSortChange = this.handleSortChange.bind(this);
+    const handleSortChange = (event) => {
+        const field = event.target.name;
+        props.sort_games(field);
     }
 
-    handleSortChange(field) {
-        this.props.sort_games(field);
+    const { state: sorters } = props;
+
+    // For Popover
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+    
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
+    // label
+    const field_labels = {
+        "name": "Trier par nom",
+        "releaseDate": "Trier par date de sortie"
     }
 
-    render() {
-
-        const { state: sorters } = this.props;
-
-        // For Popover
-        const [anchorEl, setAnchorEl] = React.useState(null);
-
-        const handleClick = (event) => {
-          setAnchorEl(event.currentTarget);
-        };
-      
-        const handleClose = () => {
-          setAnchorEl(null);
-        };
-      
-        const open = Boolean(anchorEl);
-        const id = open ? 'simple-popover' : undefined;
-
-        // label
-        const field_labels = {
-            "name": "Trier par nom",
-            "releaseDate": "Trier par date de sortie"
-        }
-
-        return (
-            <Box
-                display="flex"
-                flexWrap="wrap"
-                flexDirection="row"
-                justifyContent="flex-end"
+    return (
+        <Box
+            display="flex"
+            flexWrap="wrap"
+            flexDirection="row"
+            justifyContent="flex-end"
+        >
+            <Button aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
+                Tris
+            </Button>
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'center',
+                }}
+                transformOrigin={{
+                vertical: 'top',
+                horizontal: 'center',
+                }}
             >
-                <Button aria-describedby={id} variant="contained" color="primary" onClick={handleClick}>
-                    Tris
-                </Button>
-                <Popover
-                    id={id}
-                    open={open}
-                    anchorEl={anchorEl}
-                    onClose={handleClose}
-                    anchorOrigin={{
-                    vertical: 'bottom',
-                    horizontal: 'center',
-                    }}
-                    transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'center',
-                    }}
-                >
-                    <FormControl component="fieldset">
-                        <FormGroup>
-                            {
-                                sorters
-                                    .keys
-                                    .map(criteria => 
-                                        <FormControlLabel
-                                            control={
-                                                <>
-                                                    <Switch 
-                                                        checked={
-                                                            sorters.state[criteria] !== "ASC"
-                                                        } 
-                                                        onChange={this.handleSortChange.bind(this, criteria)} 
-                                                        field={criteria} 
-                                                    />
-                                                    {
-                                                        sorters.state[criteria] === "ASC" ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />
-                                                    }
-                                                </>
-                                            }
-                                            key={"searchCriteria_"+criteria}
-                                            label={field_labels[criteria]}
-                                        />
-                                    )
-                            }
-                        </FormGroup>
-                    </FormControl>
-                </Popover>
-            </Box>
-        )
-    }
+                <FormControl component="fieldset">
+                    <FormGroup>
+                        {
+                            sorters
+                                .keys
+                                .map(criteria => 
+                                    <FormControlLabel
+                                        control={
+                                            <>
+                                                <Switch 
+                                                    checked={
+                                                        sorters.state[criteria] !== "ASC"
+                                                    } 
+                                                    onChange={handleSortChange} 
+                                                    name={criteria} 
+                                                />
+                                                {
+                                                    sorters.state[criteria] === "ASC" ? <ArrowDropDownIcon /> : <ArrowDropUpIcon />
+                                                }
+                                            </>
+                                        }
+                                        key={"searchCriteria_"+criteria}
+                                        label={field_labels[criteria]}
+                                    />
+                                )
+                        }
+                    </FormGroup>
+                </FormControl>
+            </Popover>
+        </Box>
+    )
 }
 
 // mapStateToProps(state, ownProps)
