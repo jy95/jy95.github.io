24a6c39f58fe84dbd86d4cef7fcf0f4c9a8ade33
jy95 <jacques.yakoub@gmail.com>
2020-11-15 20:19:39 +0100

    Allow dark mode

--- src/components/Root.js
+++ src/components/Root.js
@@ -2,6 +2,10 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
+// Dark mode
+import useMediaQuery from '@material-ui/core/useMediaQuery';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
 import Header from "./Home/Header";
 import Menu from "./Home/Menu"
 import Player from "./YTPlayer/Player";
@@ -14,29 +18,43 @@ import basicStyle from "./Home/styles"
 function Root({ store }) {
 
     const classes = basicStyle();
+    const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
 
     // for drawser
     const [open, setOpen] = React.useState(false);
 
+    // Prepare theme for possible darkmode
+    const theme = React.useMemo(
+        () =>
+          createMuiTheme({
+            palette: {
+              type: prefersDarkMode ? 'dark' : 'light',
+            },
+          }),
+        [prefersDarkMode],
+      );
+
     return (
-        <div className={classes.root}>
-            <Provider store={store}>
-                {/* https://github.com/facebook/create-react-app/issues/1765#issuecomment-327615099 */}
-                <Router basename={process.env.PUBLIC_URL} >
-                    <Header open={open} setOpen={setOpen} classes={classes}/>
-                    <Menu open={open} setOpen={setOpen} classes={classes}/>
-                    <main className={classes.content}>
-                        <div className={classes.toolbar} />
-                        <Grid container>
-                            <Route path="/games" component={GamesGallery} />
-                            <Route path="/playlist/:id" component={Player} />
-                            <Route path="/video/:id" component={Player} />
-                            <Route path="/planning" component={Planning} />
-                        </Grid>
-                    </main>
-                </Router>
-            </Provider>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className={classes.root}>
+                <Provider store={store}>
+                    {/* https://github.com/facebook/create-react-app/issues/1765#issuecomment-327615099 */}
+                    <Router basename={process.env.PUBLIC_URL} >
+                        <Header open={open} setOpen={setOpen} classes={classes}/>
+                        <Menu open={open} setOpen={setOpen} classes={classes}/>
+                        <main className={classes.content}>
+                            <div className={classes.toolbar} />
+                            <Grid container>
+                                <Route path="/games" component={GamesGallery} />
+                                <Route path="/playlist/:id" component={Player} />
+                                <Route path="/video/:id" component={Player} />
+                                <Route path="/planning" component={Planning} />
+                            </Grid>
+                        </main>
+                    </Router>
+                </Provider>
+            </div>
+        </ThemeProvider>
     )
 }
 
