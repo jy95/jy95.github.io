519592defa38784dcb1aae70588334f3e1c022b9
jy95 <jacques.yakoub@gmail.com>
2021-01-24 10:11:15 +0100

    refactor: transform class to function
    So that I can use hooks instead of injecting context

--- src/components/Planning/Planning.js
+++ src/components/Planning/Planning.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import { withTranslation } from 'react-i18next';
+import {useTranslation} from "react-i18next";
+
 import {get_scheduled_games} from "../../actions/planning";
 import iconsSVG from "../GamesView/PlatformIcons";
 
@@ -13,94 +14,97 @@ import CircularProgress from '@material-ui/core/CircularProgress';
 import SvgIcon from '@material-ui/core/SvgIcon';
 import CenteredGrid from "../Others/CenteredGrid";
 
-class Viewer extends React.Component {
+// columns definitions
+const getTableColumns = (t, date_options) => [
+    {
+        field: "title", 
+        headerName: t("planning.columns.title"),
+        headerAlign: 'center',
+        renderCell: (params) => (
+            <Tooltip title={params.value} aria-label={params.value}>
+                <div>
+                    {params.value}
+                </div>
+            </Tooltip>
+        ),
+        width: 270
+    },
+    {
+        field: "platform",
+        headerName: t("planning.columns.platform"),
+        //headerAlign: 'center',
+        //align: 'center',
+        renderCell: (params) => (
+            <SvgIcon titleAccess={params.value}>
+                <path d={iconsSVG[params.value]} />
+            </SvgIcon>
+        )
+    },
+    {
+        field: "releaseDate", 
+        headerName: t("planning.columns.releaseDate"),
+        headerAlign: 'center',
+        renderCell: (params) => (
+            <>
+                {params.value.toLocaleDateString(undefined, date_options)}
+            </>
+        ),
+        width: 200
+    },
+    {
+        field: "status",
+        headerName: t("planning.columns.status"),
+        //headerAlign: 'center',
+        renderCell: (params) => (
+            <Tooltip title={t("planning.states." + params.value )} aria-label={params.value}>
+                {
+                    (() => {
+                        switch(params.value) {
+                            case "RECORDED":
+                                return <CheckCircleIcon />;
+                            case "PENDING":
+                                return <HourglassEmptyIcon />;
+                            default:
+                                return null;
+                        }
+                    })()
+                }   
+            </Tooltip>
+        )
+    }
+];
 
-    componentDidMount() {
-        this.props.get_scheduled_games();
-    };
+function Viewer(props) {
 
-    render() {
+    const {loading, data} = props;
+    const { t } = useTranslation('common');
 
-        const {loading, data, t} = this.props;
-        const date_options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    // on mount, load data (only once)
+    React.useEffect(() => {
+        props.get_scheduled_games();
+    },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        []
+    )
 
-        if (loading) {
-            return <CenteredGrid>
-                <CircularProgress/>
-            </CenteredGrid>
-        }
+    const date_options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+    const columns = getTableColumns(t, date_options);
 
-        const columns = [
-            {
-                field: "title", 
-                headerName: t("planning.columns.title"),
-                headerAlign: 'center',
-                renderCell: (params) => (
-                    <Tooltip title={params.value} aria-label={params.value}>
-                        <div>
-                            {params.value}
-                        </div>
-                    </Tooltip>
-                ),
-                width: 270
-            },
-            {
-                field: "platform",
-                headerName: t("planning.columns.platform"),
-                //headerAlign: 'center',
-                //align: 'center',
-                renderCell: (params) => (
-                    <SvgIcon titleAccess={params.value}>
-                        <path d={iconsSVG[params.value]} />
-                    </SvgIcon>
-                )
-            },
-            {
-                field: "releaseDate", 
-                headerName: t("planning.columns.releaseDate"),
-                headerAlign: 'center',
-                renderCell: (params) => (
-                    <>
-                        {params.value.toLocaleDateString(undefined, date_options)}
-                    </>
-                ),
-                width: 200
-            },
-            {
-                field: "status",
-                headerName: t("planning.columns.status"),
-                //headerAlign: 'center',
-                renderCell: (params) => (
-                    <Tooltip title={t("planning.states." + params.value )} aria-label={params.value}>
-                        {
-                            (() => {
-                                switch(params.value) {
-                                    case "RECORDED":
-                                        return <CheckCircleIcon />;
-                                    case "PENDING":
-                                        return <HourglassEmptyIcon />;
-                                    default:
-                                        return null;
-                                }
-                            })()
-                        }   
-                    </Tooltip>
-                )
-            }
-        ]
+    if (loading) {
+        return <CenteredGrid>
+            <CircularProgress/>
+        </CenteredGrid>
+    }
 
-
-        return (
-            <div style={{ height: 450, width: '100%' }}>
-                <div style={{ display: 'flex', height: '100%' }}>
-                    <div style={{ flexGrow: 1 }}>
-                        <DataGrid rows={data} columns={columns} disableSelectionOnClick/>
-                    </div>
+    return (
+        <div style={{ height: 450, width: '100%' }}>
+            <div style={{ display: 'flex', height: '100%' }}>
+                <div style={{ flexGrow: 1 }}>
+                    <DataGrid rows={data} columns={columns} disableSelectionOnClick/>
                 </div>
             </div>
-        )
-
-    }
+        </div>
+    )
 
 }
 
@@ -118,6 +122,4 @@ const mapDispatchToProps = {
 export default connect(
     mapStateToProps,
     mapDispatchToProps
-)(
-    withTranslation("common")(Viewer)
-);
+)(Viewer);
