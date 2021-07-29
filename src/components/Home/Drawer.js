75653389e02b8fc5dddc317a7939f68c5bdddb5b
jy95 <jacques.yakoub@gmail.com>
2021-07-29 22:39:22 +0200

    chore(br) : move styled drawer stuff into a proper module

---
+++ src/components/Home/Drawer.js
@@ -0,0 +1,54 @@
+import { styled } from '@material-ui/styles';
+import MuiDrawer from '@material-ui/core/Drawer';
+
+// drawerWidth
+export const drawerWidth = 240;
+
+// styled drawer header
+export const DrawerHeader = styled('div')(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+}));
+
+// Drawer Mixins
+export const openedMixin = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+});
+  
+export const closedMixin = (theme) => ({
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(${theme.spacing(9)} + 1px)`,
+    },
+});
+
+// styled Drawer
+export const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+      boxSizing: 'border-box',
+      ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+      }),
+      ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+      }),
+    }),
+);
--- src/components/Home/Menu.js
+++ src/components/Home/Menu.js
@@ -4,14 +4,12 @@ import {useTranslation} from "react-i18next";
 
 // Material UI
 import Divider from '@material-ui/core/Divider';
-import MuiDrawer from '@material-ui/core/Drawer';
 import IconButton from '@material-ui/core/IconButton';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import Tooltip from '@material-ui/core/Tooltip';
-import { styled } from '@material-ui/styles';
 
 // Router
 import {
@@ -28,6 +26,9 @@ import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 // Redux action
 import {setDrawerOpen} from "../../actions/miscellaneous"
+
+// Styled components
+import { Drawer, DrawerHeader } from "./Drawer";
 
 // List Item
 function ListItemLink(props) {
@@ -80,58 +81,6 @@ const ENTRIES = [
         "to": "/latest"
     }
 ]
-
-// drawerWidth
-const drawerWidth = 240;
-
-// styled drawer header
-const DrawerHeader = styled('div')(({ theme }) => ({
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: theme.spacing(0, 1),
-    // necessary for content to be below app bar
-    ...theme.mixins.toolbar,
-}));
-
-// Drawer Mixins
-const openedMixin = (theme) => ({
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-});
-  
-const closedMixin = (theme) => ({
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    overflowX: 'hidden',
-    width: `calc(${theme.spacing(7)} + 1px)`,
-    [theme.breakpoints.up('sm')]: {
-      width: `calc(${theme.spacing(9)} + 1px)`,
-    },
-});
-
-// styled Drawer
-const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    ({ theme, open }) => ({
-      width: drawerWidth,
-      flexShrink: 0,
-      whiteSpace: 'nowrap',
-      boxSizing: 'border-box',
-      ...(open && {
-        ...openedMixin(theme),
-        '& .MuiDrawer-paper': openedMixin(theme),
-      }),
-      ...(!open && {
-        ...closedMixin(theme),
-        '& .MuiDrawer-paper': closedMixin(theme),
-      }),
-    }),
-);
 
 // Main component
 function Menu(props) {
