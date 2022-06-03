918d0a1e43bce4d8ecdbfcf9bde07b4e652dd7f0
jy95 <jacques.yakoub@gmail.com>
2022-06-03 18:06:17 +0200

    fix: scrolling

--- src/reducers/games.tsx
+++ src/reducers/games.tsx
@@ -67,7 +67,7 @@ export default function games(state = initialState, action) {
                 initialLoad: false,
                 games: action.games,
                 totalItems: action.totalItems,
-                currentItemCount: 0,
+                currentItemCount: action.pageSize,
                 pageSize: action.pageSize,
                 error: null
             };
