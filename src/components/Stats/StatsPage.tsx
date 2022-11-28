d9fe654cd67e638fa8a75a63cc6ae10d53fe507b
jy95 <jacques.yakoub@gmail.com>
2022-11-28 22:41:40 +0100

    chore: first chart

--- src/components/Stats/StatsPage.tsx
+++ src/components/Stats/StatsPage.tsx
@@ -1,4 +1,6 @@
 import { useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar } from 'recharts';
 
 // Custom
 import ReloadWrapper from "../Others/ReloadWrapper";
@@ -6,6 +8,9 @@ import ReloadWrapper from "../Others/ReloadWrapper";
 import { fetchStats, selectStats } from "../../services/statsSlice";
 // Hooks
 import { useAppDispatch, useAppSelector } from "../../hooks/typedRedux";
+
+// types
+import type { Genre as GenreValue } from "../../services/sharedDefintion";
 
 // The gallery component
 function StatsPage(_props : {[key: string | number | symbol] : any}) {
@@ -16,6 +21,7 @@ function StatsPage(_props : {[key: string | number | symbol] : any}) {
         error,
         stats
     } = useAppSelector((state) => selectStats(state));
+    const { t } = useTranslation('common');
 
     // on mount, load data (only once)
     useEffect(() => {
@@ -25,7 +31,11 @@ function StatsPage(_props : {[key: string | number | symbol] : any}) {
         []
     );
 
-    console.log(stats);
+    // for genre chart
+    const genresData = stats.genres.map(s => ({
+        ...s,
+        category: t(`gamesLibrary.gamesGenres.${s.key as GenreValue}` as const)
+    }));
 
     // TODO
     return (
@@ -34,7 +44,13 @@ function StatsPage(_props : {[key: string | number | symbol] : any}) {
             error={error}
             reloadFct={() => {dispatch(fetchStats());}}
             component={
-                <div />
+                <RadarChart outerRadius={90} width={500} height={500} data={genresData}>
+                    <PolarGrid />
+                    <PolarAngleAxis dataKey="category" />
+                    <PolarRadiusAxis angle={30} />
+                    <Radar name="Mike" dataKey="total_available" stroke="#1fa134" fill="#1fa134" fillOpacity={0.6} />
+                    <Radar name="Mike" dataKey="total_unavailable" stroke="#8faaba" fill="#8faaba" fillOpacity={0.6} />
+                </RadarChart>
             }
         />
     );
