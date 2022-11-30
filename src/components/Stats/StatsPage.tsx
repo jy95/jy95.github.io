da3bcd39ad9a99d02e5b21ea14b76dbb75410792
jy95 <jacques.yakoub@gmail.com>
2022-11-30 21:53:40 +0100

    chore: find settings to make tooltip readable

--- src/components/Stats/StatsPage.tsx
+++ src/components/Stats/StatsPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
-import { RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, Radar } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts';
 
 // Custom
 import ReloadWrapper from "../Others/ReloadWrapper";
@@ -15,13 +15,14 @@ import type { Genre as GenreValue } from "../../services/sharedDefintion";
 // The gallery component
 function StatsPage(_props : {[key: string | number | symbol] : any}) {
 
+    const { t } = useTranslation('common');
     const dispatch = useAppDispatch();
     const {
         loading,
         error,
         stats
     } = useAppSelector((state) => selectStats(state));
-    const { t } = useTranslation('common');
+    const currentColor = useAppSelector( (state) => state.themeColor.currentColor )
 
     // on mount, load data (only once)
     useEffect(() => {
@@ -40,6 +41,11 @@ function StatsPage(_props : {[key: string | number | symbol] : any}) {
     // for platform chart
     const platformsData = stats.platforms;
 
+    // StackedAreaChart : https://recharts.org/en-US/examples/StackedAreaChart
+    // StackedBarChart : https://recharts.org/en-US/examples/StackedBarChart
+
+    const strokeColor = (currentColor === "dark") ? "white": "dark";    
+
     // TODO
     return (
         <ReloadWrapper  
@@ -50,23 +56,23 @@ function StatsPage(_props : {[key: string | number | symbol] : any}) {
                 <>
                 {
                     genresData.length > 0 &&
-                    <RadarChart outerRadius={90} width={500} height={250} data={genresData}>
-                        <PolarGrid />
-                        <PolarAngleAxis dataKey="category" />
-                        <PolarRadiusAxis angle={30} />
-                        <Radar name="Mike" dataKey="total_available" stroke="#1fa134" fill="#1fa134" fillOpacity={0.6} />
-                        <Radar name="Mike" dataKey="total_unavailable" stroke="#8faaba" fill="#8faaba" fillOpacity={0.6} />
-                    </RadarChart>
+                    <BarChart width={500} height={250} data={genresData}>
+                        <CartesianGrid strokeDasharray="2 2" />
+                        <XAxis dataKey="category" stroke={strokeColor} />
+                        <YAxis stroke={strokeColor}  />
+                        <Tooltip contentStyle={{backgroundColor: currentColor}} />
+                        <Bar type="monotone" dataKey="total_available" stackId="1" stroke="#82ca9d" fill="#82ca9d" name={t("stats.genresChart.total_available")} />
+                        <Bar type="monotone" dataKey="total_unavailable" stackId="1" stroke="#8884d8" fill="#8884d8" name={t("stats.genresChart.total_unavailable")} />
+                    </BarChart>
                 }
-                { 
+                { /*
                     platformsData.length > 0 &&
                     <RadarChart outerRadius={90} width={300} height={250} data={platformsData}>
                         <PolarGrid />
                         <PolarAngleAxis dataKey="key" />
-                        <PolarRadiusAxis angle={30} />
                         <Radar name="Mike" dataKey="total_available" stroke="#1fa134" fill="#1fa134" fillOpacity={0.6} />
-                        <Radar name="Mike" dataKey="total_unavailable" stroke="#8faaba" fill="#8faaba" fillOpacity={0.6} />
-                    </RadarChart>
+                        <Radar name="Mike" dataKey="total_unavailable" stroke="#ff0000" fill="#ff0000" fillOpacity={0.6} />
+                    </RadarChart> */
                 }
                 </>
             }
