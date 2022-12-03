4b6099500ef129285c8ddbe3bafc5462f61da40a
jy95 <jacques.yakoub@gmail.com>
2022-12-03 20:51:00 +0100

    chore: add more numbers

--- src/components/Stats/StatsPage.tsx
+++ src/components/Stats/StatsPage.tsx
@@ -31,6 +31,56 @@ import { useAppDispatch, useAppSelector } from "../../hooks/typedRedux";
 // types
 import type { Genre as GenreValue } from "../../services/sharedDefintion";
 
+// Inspired by https://blog.bitsrc.io/calculate-the-difference-between-two-2-dates-e1d76737c05a
+// My version includes some improvements in the codebase & changes to fit my needs
+function calcDate(date1 : string,  t : (key : string, options?: any) => string) {
+
+    //new date instance
+    const dt_date1 = new Date(date1);
+    const dt_date2 = new Date();
+
+    //Get the Timestamp
+    const date1_time_stamp = dt_date1.getTime();
+    const date2_time_stamp = dt_date2.getTime();
+
+    let calc;
+
+    //Check which timestamp is greater
+    if (date1_time_stamp > date2_time_stamp) {
+        calc = new Date(date1_time_stamp - date2_time_stamp);
+    } else {
+        calc = new Date(date2_time_stamp - date1_time_stamp);
+    }
+    //Retrieve the date, month and year
+    const calcFormatTmp = calc.getDate() + '-' + (calc.getMonth() + 1) + '-' + calc.getFullYear();
+    //Convert to an array and store
+    const calcFormat = calcFormatTmp.split("-");
+    //Subtract each member of our array from the default date
+    const days_passed = Number(Math.abs(Number(calcFormat[0])) - 1);
+    const months_passed = Number(Math.abs(Number(calcFormat[1])) - 1);
+    const years_passed = Number(Math.abs(Number(calcFormat[2])) - 1970);
+
+    //Convert to days and sum together
+    const total_days = (years_passed * 365) + (months_passed * 30.417) + days_passed;
+    const total_secs = total_days * 24 * 60 * 60;
+    const total_mins = total_days * 24 * 60;
+    const total_hours = total_days * 24;
+    const total_weeks = ( total_days >= 7 ) ? total_days / 7 : 0;
+
+    //display result with custom text
+    const result = `${t("common.dates.years", {count: years_passed})} ${t("common.dates.months", {count: months_passed})} ${t("common.dates.days", {count: days_passed})}`
+
+    //return the result
+    return {
+        "total_days": Math.round(total_days),
+        "total_weeks": Math.round(total_weeks),
+        "total_hours" : Math.round(total_hours),
+        "total_minutes" : Math.round(total_mins),
+        "total_seconds": Math.round(total_secs),
+        "result": result.trim()
+    }
+}
+
 // The gallery component
 function StatsPage(_props : {[key: string | number | symbol] : any}) {
 
@@ -86,7 +136,20 @@ function StatsPage(_props : {[key: string | number | symbol] : any}) {
                         >
                             <Typography component="h2" variant="h6" color="primary" gutterBottom>{t("stats.generalStats.title")}</Typography>
                             <List>
-                                <ListItem>{`${t("stats.generalStats.total_games")} : ${generalStats.total} ${t("stats.generalStats.total_games_details", {available : generalStats.total_available, not_available: generalStats.total_unavailable})}` }</ListItem>
+                                <ListItem>{`${
+                                    t("stats.generalStats.total_games")
+                                } : ${generalStats.total} ${
+                                    t("stats.generalStats.total_games_details", {available : generalStats.total_available, not_available: generalStats.total_unavailable})}` 
+                                }
+                                </ListItem>
+                                <ListItem>{`${
+                                    t("stats.generalStats.channel_start_date")
+                                } : ${
+                                    new Date(generalStats.channel_start_date).toLocaleDateString()
+                                } ${
+                                    t("stats.generalStats.channel_start_date_details", {value: calcDate(generalStats.channel_start_date, t).result})
+                                }`}
+                                </ListItem>
                             </List>
                         </Paper>
                     </Grid>
