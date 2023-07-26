af21e0ad7f8da1ec80aa7a40f085a496d1208ae8
jy95 <jacques.yakoub@gmail.com>
2023-07-26 22:50:57 +0200

    chore: add player pages

---
+++ src/app/[lng]/playlist/[id]/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { useParams } from 'next/navigation'
+import YTPlayer from "@/components/YTPlayer/Player";
+
+export default function PlaylistPage() {
+
+    const { id } = useParams();
+    const identifier = id as string;
+
+    return (
+        <YTPlayer type="PLAYLIST" identifier={identifier}/>
+    )
+}
---
+++ src/components/YTPlayer/Player.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { lazy } from 'react'
+const ReactPlayer = lazy(() => import("react-player/youtube"));
+
+type Params = {type: "PLAYLIST" | "VIDEO", identifier : string}
+
+export default function Player({type, identifier} : Params) {
+    const url = (type === "PLAYLIST") 
+        ? `https://www.youtube.com/playlist?list=${identifier}` 
+        : `https://www.youtube.com/watch?v=${identifier}`;
+    
+    return (
+        <ReactPlayer
+            controls={true}
+            url={url}
+            playing={true}
+            width='100%'
+            height='90vh'
+        />
+    );
+}
