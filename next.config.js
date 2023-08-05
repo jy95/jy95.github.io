96f836a816a1d6edc26ff2f75ff3119f23e9def2
jy95 <jacques.yakoub@gmail.com>
2023-08-05 20:40:54 +0200

    chore: set output export of nextjs

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const nextTranslate = require('next-translate-plugin')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+    // Github pages is a static host
+    output: "export"
 }
 
 // Or if you already have next.config.js file and want to keep the changes in it, pass the config object to the nextTranslate()
