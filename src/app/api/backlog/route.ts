a38dfdd07396396ab6ae1448cbf358edb894b0e0
jy95 <jacques.yakoub@gmail.com>
2024-03-03 18:17:09 +0100

    fix: add row id for backlog

--- src/app/api/backlog/route.ts
+++ src/app/api/backlog/route.ts
@@ -4,7 +4,10 @@ import type {
     Platform
 } from "@/redux/sharedDefintion";
 
+// An entry of backlog
 export type BacklogEntry = {
+    /**@description Id */
+    "id": string,
     /** @description Name of the game */
     "title": string,
     /** @description Platform for that game */
@@ -12,6 +15,9 @@ export type BacklogEntry = {
     /** @description Extra notes */
     "notes"?: string
 }
+
+// Raw entry 
+type RawBacklogEntry = Exclude<BacklogEntry, "id">;
 
 // Revalidate at most every day
 // https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate
@@ -22,7 +28,16 @@ export const revalidate = 86400;
 export async function GET() {
 
     // Game data
-    const gamesData = (await import("./backlog.json")).default as BacklogEntry[];
+    const gamesData = (await import("./backlog.json")).default;
+    const games = gamesData.map( (game, idx) => enhanceGameItem(game as RawBacklogEntry, idx) );
 
     return NextResponse.json(gamesData);
+}
+
+// Return an enhanced payload for a single game
+function enhanceGameItem(game: RawBacklogEntry, idx: number): BacklogEntry {
+    return Object.assign({}, game, {
+        // MUI: The data grid component requires all rows to have a unique `id` property.
+        "id": idx.toString()
+    })
 }
