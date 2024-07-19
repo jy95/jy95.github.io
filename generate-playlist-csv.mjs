00128eb19a921c73e20102d7d70a9704b2dd51bc
jy95 <jacques.yakoub@gmail.com>
2024-07-20 00:58:21 +0200

    feat: filter by playlist

--- generate-playlist-csv.mjs
+++ generate-playlist-csv.mjs
@@ -10,9 +10,35 @@ import { google } from "googleapis";
 import input from '@inquirer/input';
 
 const scope = ["https://www.googleapis.com/auth/youtube.readonly"];
-const START_DATE = "2023-09-01";
-const END_DATE = "2024-03-31";
-const MAX_RESULTS = 50;
+const START_DATE = "2023-01-01";
+const END_DATE = "2024-12-31";
+const MAX_RESULTS = 500;
+
+// See "Statistics for a specific playlist"
+// https://developers.google.com/youtube/analytics/sample-requests
+const generateFilters = (playListIds) => `playlist==${playListIds.join(",")}`;
+/*
+const FILTERS = [
+  "PLRfhDHeBTBJ6sm2gbS9qtdovdSGygP-3f",
+  "PLRfhDHeBTBJ44bSU0cT2G0m4srnTO2gZY",
+  "PLRfhDHeBTBJ49XIAdTq1YJ5ozgKjaUM6E",
+  "PLRfhDHeBTBJ6L_uo8nMekfbJRE_ixUG5H",
+  "PLRfhDHeBTBJ4ZxkrGCk7a8C-6jXEj4HoT",
+  "PLRfhDHeBTBJ5hLNQ3UBKtImQLXkR1bChZ",
+  "PLRfhDHeBTBJ6zkB2GAtg_k6_J799AozQz",
+  "PLRfhDHeBTBJ6FOWzD8YONj2LoxCxPNJzO",
+  "PLRfhDHeBTBJ6aLNhmxhZdrI2dz0Bavlkh",
+  "PLRfhDHeBTBJ4GoSaMs-BqHumiPqUHU3OX",
+  "PLRfhDHeBTBJ6PHF-BH9dFug86rb877o2y",
+  "PLRfhDHeBTBJ47XmS2_upaFjIh_wKhN0XB",
+  "PLRfhDHeBTBJ7dIG6eD6CYJh8xu4j_eXyq",
+  "PLRfhDHeBTBJ5K0xPqtndkObR9g05hHw5h",
+  "PLRfhDHeBTBJ54lUeJrodUPuEOohHUdCJj",
+  "PLRfhDHeBTBJ72gRyZBPf23ZfxSlG5h8Em",
+  "PLRfhDHeBTBJ5BASLY5Kgof-1fgRxNVkOX",
+  "PLRfhDHeBTBJ5EYj5y6ByrLTuhqyKV4dpc"
+];
+*/
 
 // Load client secrets from a local file.
 // https://stackoverflow.com/a/52222827/6149867
@@ -61,6 +87,7 @@ const callApi = async (auth) => {
       sort: "-estimatedMinutesWatched",
       startDate: START_DATE,
       endDate: END_DATE
+      //filters: generateFilters(FILTERS)
     });
 
     const rows = response.data.rows || [];
