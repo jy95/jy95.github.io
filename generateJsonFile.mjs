0b96ec9c7e6868dc65665ce3a76add69a8ddc064
jy95 <jacques.yakoub@gmail.com>
2024-08-12 23:54:27 +0200

    chore: put information into database (WIP)

--- generateJsonFile.mjs
+++ generateJsonFile.mjs
@@ -1,5 +1,4 @@
-import { readFile, writeFile, access } from "fs/promises";
-import { resolve as resolvePath } from "path";
+import { writeFile } from "fs/promises";
 import Database from 'better-sqlite3';
 
 // Params
@@ -11,7 +10,8 @@ const FILES = {
     "TESTS": "src/app/api/tests/tests.json",
     "PLATFORMS": "src/app/api/platforms/platforms.json",
     "GENRES": "src/app/api/genres/genres.json",
-    "PLANNING": "src/app/api/planning/planning.json"
+    "PLANNING": "src/app/api/planning/planning.json",
+    "STATS": "src/app/api/stats/stats.json"
 }
 
 const db = new Database(databasePath, {
@@ -47,89 +47,146 @@ function normaliazeDuration(duration) {
 
 }
 
-// Extract platforms
-const extractPlatformsStmt = db.prepare("SELECT id, name FROM platforms");
-const platforms = extractPlatformsStmt.all();
-await writeFile(
-    FILES.PLATFORMS,
-    stringifyJSON(platforms),
-    "utf-8"
-);
 
-// Extract genres
-const extractGenresStmt = db.prepare("SELECT id, name FROM genres");
-const genres = extractGenresStmt.all();
-await writeFile(
-    FILES.GENRES,
-    stringifyJSON(genres),
-    "utf-8"
-);
-
-// Extract backlog
-const extractBacklogStmt = db.prepare("SELECT id, title, platform, notes FROM backlog");
-const backlog = extractBacklogStmt.all();
-await writeFile(
-    FILES.BACKLOG,
-    stringifyJSON(backlog),
-    "utf-8"
-);
-
-// Extract planning
-const extractPlanningStmt = db.prepare("SELECT * FROM games_in_future gif INNER JOIN games g ON g.id == gif.id");
-const planning = extractPlanningStmt.all();
-await writeFile(
-    FILES.PLANNING,
-    stringifyJSON(planning),
-    "utf-8"
-);
-
-// Extract games
-const extractGamesList = db.prepare("SELECT * FROM games_in_present");
-const gamesList = extractGamesList.all();
-await writeFile(
-    FILES.GAMES,
-    stringifyJSON(gamesList),
-    "utf-8"
-);
-
-// Extract series
-const extractSeriesStmt = db.prepare("SELECT * FROM series_as_json");
-const series = extractSeriesStmt.all();
-await writeFile(
-    FILES.SERIES,
-    stringifyJSON(series),
-    "utf-8"
-);
-
-// Extract tests
-const extractTestsStmt = db.prepare("SELECT title, videoId, playlistId FROM tests");
-const tests = extractTestsStmt.all();
-await writeFile(
-    FILES.TESTS,
-    stringifyJSON(tests),
-    "utf-8"
-);
-
-// Extract stats
-const genresStats = db.prepare("SELECT * FROM genres_stats").all();
-const platformStats = db.prepare("SELECT * FROM platforms_stats").all();
-const games_total_time = db.prepare("SELECT * FROM games_total_time").get();
-const games_total_time_available = db.prepare("SELECT * FROM games_available_time").get();
-const games_total_time_unavailable = db.prepare("SELECT * FROM games_unavailable_time").get();
-const total_games = db.prepare("SELECT COUNT(*) FROM games").get();
-const total_game_available = db.prepare("SELECT COUNT(*) FROM games_in_present").get();
-const total_game_unavailable = db.prepare("SELECT COUNT(*) FROM games_in_future").get();
-
-const result = {
-    "platforms": platformStats,
-    "genres": genresStats,
-    "general": {
-        "total": total_games,
-        "total_available": total_game_available,
-        "total_unavailable": total_game_unavailable,
-        "channel_start_date": "2014-04-15T17:35:16+00:00",
-        "total_time": normaliazeDuration(games_total_time),
-        "total_time_available": normaliazeDuration(games_total_time_available),
-        "total_time_unavailable": normaliazeDuration(games_total_time_unavailable)
-    }
+/**
+ * Extracts platforms from the database and saves them to a file.
+ * @param {import('better-sqlite3').Database} db - The database instance
+ */
+async function extractAndSavePlatforms(db) {
+    const extractPlatformsStmt = db.prepare("SELECT id, name FROM platforms");
+    const platforms = extractPlatformsStmt.all();
+    await writeFile(
+        FILES.PLATFORMS,
+        stringifyJSON(platforms),
+        "utf-8"
+    );
 }
+
+/**
+ * Extracts genres from the database and saves them to a file.
+ * @param {import('better-sqlite3').Database} db - The database instance
+ */
+async function extractAndSaveGenres(db) {
+    const extractGenresStmt = db.prepare("SELECT id, name FROM genres");
+    const genres = extractGenresStmt.all();
+    await writeFile(
+        FILES.GENRES,
+        stringifyJSON(genres),
+        "utf-8"
+    );
+}
+
+/**
+ * Extracts backlog from the database and saves them to a file.
+ * @param {import('better-sqlite3').Database} db - The database instance
+ */
+async function extractAndSaveBacklog(db) {
+    const extractBacklogStmt = db.prepare("SELECT id, title, platform, notes FROM backlog");
+    const backlog = extractBacklogStmt.all();
+    await writeFile(
+        FILES.BACKLOG,
+        stringifyJSON(backlog),
+        "utf-8"
+    );
+}
+
+/**
+ * Extracts planning from the database and saves them to a file.
+ * @param {import('better-sqlite3').Database} db - The database instance
+ */
+async function extractAndSavePlanning(db) {
+    const extractPlanningStmt = db.prepare("SELECT * FROM games_in_future gif INNER JOIN games g ON g.id == gif.id");
+    const planning = extractPlanningStmt.all();
+    await writeFile(
+        FILES.PLANNING,
+        stringifyJSON(planning),
+        "utf-8"
+    );
+}
+
+/**
+ * Extracts games from the database and saves them to a file.
+ * @param {import('better-sqlite3').Database} db - The database instance
+ */
+async function extractAndSaveGames(db) {
+    const extractGamesList = db.prepare("SELECT * FROM games_in_present");
+    const gamesList = extractGamesList.all();
+    await writeFile(
+        FILES.GAMES,
+        stringifyJSON(gamesList),
+        "utf-8"
+    );    
+}
+
+/**
+ * Extracts series from the database and saves them to a file.
+ * @param {import('better-sqlite3').Database} db - The database instance
+ */
+async function extractAndSaveSeries(db) {
+    const extractSeriesStmt = db.prepare("SELECT * FROM series_as_json");
+    const series = extractSeriesStmt.all();
+    await writeFile(
+        FILES.SERIES,
+        stringifyJSON(series),
+        "utf-8"
+    );
+}
+
+/**
+ * Extracts tests from the database and saves them to a file.
+ * @param {import('better-sqlite3').Database} db - The database instance
+ */
+async function extractAndSaveTests(db) {
+    const extractTestsStmt = db.prepare("SELECT title, videoId, playlistId FROM tests");
+    const tests = extractTestsStmt.all();
+    await writeFile(
+        FILES.TESTS,
+        stringifyJSON(tests),
+        "utf-8"
+    );    
+}
+
+/**
+ * Extracts stats from the database and saves them to a file.
+ * @param {import('better-sqlite3').Database} db - The database instance
+ */
+async function extractAndSaveStats(db) {
+    const genresStats = db.prepare("SELECT * FROM genres_stats").all();
+    const platformStats = db.prepare("SELECT * FROM platforms_stats").all();
+    const games_total_time = db.prepare("SELECT * FROM games_total_time").get();
+    const games_total_time_available = db.prepare("SELECT * FROM games_available_time").get();
+    const games_total_time_unavailable = db.prepare("SELECT * FROM games_unavailable_time").get();
+    const total_games = db.prepare("SELECT COUNT(*) FROM games").get();
+    const total_game_available = db.prepare("SELECT COUNT(*) FROM games_in_present").get();
+    const total_game_unavailable = db.prepare("SELECT COUNT(*) FROM games_in_future").get();
+    
+    const result = {
+        "platforms": platformStats,
+        "genres": genresStats,
+        "general": {
+            "total": total_games,
+            "total_available": total_game_available,
+            "total_unavailable": total_game_unavailable,
+            "channel_start_date": "2014-04-15T17:35:16+00:00",
+            "total_time": normaliazeDuration(games_total_time),
+            "total_time_available": normaliazeDuration(games_total_time_available),
+            "total_time_unavailable": normaliazeDuration(games_total_time_unavailable)
+        }
+    }
+
+    await writeFile(
+        FILES.STATS,
+        stringifyJSON(result),
+        "utf-8"
+    ); 
+}
+
+// Operations time
+await extractAndSavePlatforms(db)
+await extractAndSaveGenres(db)
+await extractAndSaveBacklog(db)
+await extractAndSavePlanning(db)
+await extractAndSaveGames(db)
+await extractAndSaveSeries(db)
+await extractAndSaveTests(db)
+await extractAndSaveStats(db)
