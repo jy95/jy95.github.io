53255d0567a774633df467307c1a170c7e77b0ca
jy95 <jacques.yakoub@gmail.com>
2024-09-19 13:22:08 +0200

    feat: refactor chart platforms

--- src/app/[locale]/stats/_client/PlatformsChart.tsx
+++ src/app/[locale]/stats/_client/PlatformsChart.tsx
@@ -11,77 +11,75 @@ import Paper from "@mui/material/Paper";
 
 // Recharts components
 import {
-    PolarAngleAxis,
-    PolarGrid,
-    Radar,
-    RadarChart,
-    ResponsiveContainer,
-    Legend,
-  } from "recharts";
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ResponsiveContainer,
+} from "recharts";
 
 // Types
 import type { statsProperty } from "@/app/api/stats/route";
 
 type Props = {
-  stats: statsProperty
-}
+  stats: statsProperty;
+};
 
-export default function PlatformsChart({stats} : Props) {
+export default function PlatformsChart({ stats }: Props) {
+  const t = useTranslations();
+  const currentColor = useAppSelector((state) => state.themeColor.currentColor);
 
-    const t = useTranslations();
-    const currentColor = useAppSelector((state) => state.themeColor.currentColor);
+  const strokeColor = currentColor === "dark" ? "white" : "dark";
+  const platformsData = stats.platforms.map((platform) => ({
+    key: platform.platform,
+    total_available: platform.total_available,
+    total_unavailable: platform.total_unavailable,
+  }));
 
-    const strokeColor = currentColor === "dark" ? "white" : "dark";
-    const platformsData = stats.platforms
-        .map(platform => ({
-            key: platform.platform,
-            ...platform
-        }));
+  if (platformsData.length === 0) {
+    return <></>; // return nothing if data is empty
+  }
 
-    if (platformsData.length === 0) {
-        return <></>; 
-    }
-
-    return (
-        <Grid item xs={12} md={4} lg={4}>
-          <Paper
-            sx={{
-              p: 2,
-              display: "flex",
-              flexDirection: "column",
-              height: 360,
-            }}
-          >
-            <Typography
-              component="h2"
-              variant="h6"
-              color="primary"
-              gutterBottom
-            >
-              {t("stats.platformsChart.title")}
-            </Typography>
-            <ResponsiveContainer>
-              <RadarChart outerRadius={90} data={platformsData}>
-                <PolarGrid />
-                <PolarAngleAxis dataKey="key" stroke={strokeColor} />
-                <Radar
-                  name={t("stats.platformsChart.total_available")}
-                  dataKey="total_available"
-                  stroke="#1fa134"
-                  fill="#1fa134"
-                  fillOpacity={0.6}
-                />
-                <Radar
-                  name={t("stats.platformsChart.total_unavailable")}
-                  dataKey="total_unavailable"
-                  stroke="#8884d8"
-                  fill="#8884d8"
-                  fillOpacity={0.6}
-                />
-                <Legend />
-              </RadarChart>
-            </ResponsiveContainer>
-          </Paper>
-        </Grid>
-    );
+  return (
+    <Grid item xs={12} md={4} lg={4}>
+      <Paper
+        sx={{
+          p: 2,
+          display: "flex",
+          flexDirection: "column",
+          height: 360,
+        }}
+      >
+        <Typography component="h2" variant="h6" color="primary" gutterBottom>
+          {t("stats.platformsChart.title")}
+        </Typography>
+        <ResponsiveContainer>
+          <AreaChart data={platformsData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="key" stroke={strokeColor} />
+            <YAxis />
+            <Tooltip contentStyle={{ backgroundColor: currentColor }} />
+            <Legend />
+            <Area
+              type="monotone"
+              dataKey="total_available"
+              stroke="#1fa134"
+              fill="#1fa134"
+              fillOpacity={0.6}
+            />
+            <Area
+              type="monotone"
+              dataKey="total_unavailable"
+              stroke="#8884d8"
+              fill="#8884d8"
+              fillOpacity={0.6}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </Paper>
+    </Grid>
+  );
 }
