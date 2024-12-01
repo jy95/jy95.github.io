7b4506df3f5dcf29e01f7a07289752ee39e85ac0
Jacques Yakoub <jacques.yakoub@gmail.com>
2024-12-01 12:53:58 +0100

    refactor: migrate redux code to be compliant with next.js (#1057)

--- src/redux/Store.tsx
+++ src/redux/Store.tsx
@@ -11,37 +11,40 @@ import { platformsAPI } from "./services/platformsAPI"
 import { genresAPI } from "./services/genresAPI";
 import { dlcsAPI } from "./services/dlcsAPI";
 
-/* eslint-disable no-underscore-dangle */
-const store = configureStore({
-    reducer: {
-        // common reducers
-        games,
-        // API calls
-        [gamesAPI.reducerPath]: gamesAPI.reducer,
-        [planningAPI.reducerPath]: planningAPI.reducer,
-        [seriesAPI.reducerPath]: seriesAPI.reducer,
-        [statsAPI.reducerPath]: statsAPI.reducer,
-        [testsAPI.reducerPath]: testsAPI.reducer,
-        [backlogAPI.reducerPath]: backlogAPI.reducer,
-        [platformsAPI.reducerPath]: platformsAPI.reducer,
-        [genresAPI.reducerPath]: genresAPI.reducer,
-        [dlcsAPI.reducerPath]: dlcsAPI.reducer
-    },
-    // Adding the api middleware enables caching, invalidation, polling,
-    // and other useful features of `rtk-query`.
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
-        .concat(gamesAPI.middleware)
-        .concat(planningAPI.middleware)
-        .concat(seriesAPI.middleware)
-        .concat(statsAPI.middleware)
-        .concat(testsAPI.middleware)
-        .concat(backlogAPI.middleware)
-        .concat(platformsAPI.middleware)
-        .concat(genresAPI.middleware)
-        .concat(dlcsAPI.middleware),
-})
-export default store;
+export const makeStore = () => {
+    return configureStore({
+        reducer: {
+            // common reducers
+            games,
+            // API calls
+            [gamesAPI.reducerPath]: gamesAPI.reducer,
+            [planningAPI.reducerPath]: planningAPI.reducer,
+            [seriesAPI.reducerPath]: seriesAPI.reducer,
+            [statsAPI.reducerPath]: statsAPI.reducer,
+            [testsAPI.reducerPath]: testsAPI.reducer,
+            [backlogAPI.reducerPath]: backlogAPI.reducer,
+            [platformsAPI.reducerPath]: platformsAPI.reducer,
+            [genresAPI.reducerPath]: genresAPI.reducer,
+            [dlcsAPI.reducerPath]: dlcsAPI.reducer
+        },
+        // Adding the api middleware enables caching, invalidation, polling,
+        // and other useful features of `rtk-query`.
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+            .concat(gamesAPI.middleware)
+            .concat(planningAPI.middleware)
+            .concat(seriesAPI.middleware)
+            .concat(statsAPI.middleware)
+            .concat(testsAPI.middleware)
+            .concat(backlogAPI.middleware)
+            .concat(platformsAPI.middleware)
+            .concat(genresAPI.middleware)
+            .concat(dlcsAPI.middleware),
+    });
+}
+
+// Infer the type of makeStore
+export type AppStore = ReturnType<typeof makeStore>
+
 // Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<AppStore['getState']>
+export type AppDispatch = AppStore['dispatch']
