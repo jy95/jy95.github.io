edc9151861de5b8bd8677a0c5a85988b66366b2d
jy95 <jacques.yakoub@gmail.com>
2024-12-03 23:49:23 +0100

    perf: generateStaticParams for player routes

--- src/app/[locale]/playlist/[id]/page.tsx
+++ src/app/[locale]/playlist/[id]/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+export async function generateStaticParams() {
+    const identifiers = (await import("@/app/api/random/identifiers.json")).default;
+    const playlists = identifiers.filter(i => i.playlistId !== undefined);
+
+    return playlists.map((pl) => ({
+        id: pl.playlistId,
+    }));
+}
+
 import { useParams } from 'next/navigation'
 import YTPlayer from "@/components/YTPlayer/Player";
 import RandomButton from '@/components/GamesView/RandomButton';
--- src/app/[locale]/video/[id]/page.tsx
+++ src/app/[locale]/video/[id]/page.tsx
@@ -1,5 +1,14 @@
 "use client";
 
+export async function generateStaticParams() {
+    const identifiers = (await import("@/app/api/random/identifiers.json")).default;
+    const videos = identifiers.filter(i => i.videoId !== undefined);
+
+    return videos.map((vid) => ({
+        id: vid.videoId,
+    }));
+}
+
 import { useParams } from 'next/navigation'
 import YTPlayer from "@/components/YTPlayer/Player";
 import RandomButton from '@/components/GamesView/RandomButton';
