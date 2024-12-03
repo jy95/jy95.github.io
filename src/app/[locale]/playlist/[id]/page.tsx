f8e4794f3a1e18759f0a84a7f01b38fc863c3266
jy95 <jacques.yakoub@gmail.com>
2024-12-04 00:33:08 +0100

    perf: convert /video and /playlist as Server Components

--- src/app/[locale]/playlist/[id]/page.tsx
+++ src/app/[locale]/playlist/[id]/page.tsx
@@ -1,4 +1,7 @@
-"use client";
+// https://nextjs.org/docs/app/api-reference/file-conventions/page#props
+type Props = {
+    params: Promise<{ id: string }>
+}
 
 export async function generateStaticParams() {
     const identifiers = (await import("@/app/api/random/identifiers.json")).default;
@@ -9,13 +12,12 @@ export async function generateStaticParams() {
     }));
 }
 
-import { useParams } from 'next/navigation'
 import YTPlayer from "@/components/YTPlayer/Player";
 import RandomButton from '@/components/GamesView/RandomButton';
 
-export default function PlaylistPage() {
+export default async function PlaylistPage({ params } : Props) {
 
-    const { id } = useParams();
+    const id = (await params).id
     const identifier = id as string;
 
     return (
