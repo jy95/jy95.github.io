e6d7b73d6cc74fa5379d6c251c4f60a027cde824
jy95 <jacques.yakoub@gmail.com>
2024-12-05 12:33:39 +0100

    perf: simplify not found

--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,9 +1,5 @@
 // https://next-intl-docs.vercel.app/docs/environments/error-files#not-foundjs
 
-// Hooks
-import {setRequestLocale} from 'next-intl/server';
-import {useTranslations} from 'next-intl';
-
 // https://next-intl-docs.vercel.app/docs/routing/navigation
 import { Link } from '@/i18n/routing';
 
@@ -12,20 +8,7 @@ import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-type Props = {
-    params: Promise<{
-      locale: "en" | "fr"
-    }>
-  }
-
-export default async function NotFoundPage({ params }: Props) {
-
-    const locale = (await params).locale;
-
-    // Enable static rendering
-    setRequestLocale(locale);
-
-    const t = useTranslations('notFound');
+export default async function NotFoundPage() {
 
     return (
         <Box
@@ -36,10 +19,10 @@ export default async function NotFoundPage({ params }: Props) {
                 justifyContent: "center",
                 height: "80vh"
             }}>
-            <Typography variant="h4" gutterBottom>{t('title')}</Typography>
+            <Typography variant="h4" gutterBottom>{"We couldn't find that page"}</Typography>
             <Link href="/">
                 <Button variant="contained" color="primary">
-                    {t('goGome')}
+                    {"Return home"}
                 </Button>
             </Link>
         </Box>
