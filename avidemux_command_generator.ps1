1d0564761528463132aeca2cf51541898f226fb2
Jacques Yakoub <jacques.yakoub@gmail.com>
2025-07-14 23:51:55 +0200

    Update avidemux_command_generator.ps1

--- avidemux_command_generator.ps1
+++ avidemux_command_generator.ps1
@@ -1,24 +1,30 @@
-# Where Avidemux cli is stored
+# Where Avidemux CLI is stored
 $avidemux_cli = "C:\Program Files\Avidemux 2.8 VC++ 64bits\avidemux_cli.exe"
 $files_folders = "D:\captures\BOSS"
-$avidemux_common_args = "-video-codec copy --audio-codec copy --output-format mkv --save output.mkv"
-# Kind of file I use
+$avidemux_common_args = @("--video-codec", "copy", "--audio-codec", "copy", "--output-format", "mkv", "--save", "output.mkv")
 $search_criteria = "*.mkv"
-# Search request, to return files in natural order (A1, ..., A10, ...)
+
+# Search files in natural order
 $foundFiles = @(
-    Get-ChildItem -Path $files_folders -filter $search_criteria |
-    Where-Object {! $_.PSIsContainer } |
-    Select-Object Name | 
+    Get-ChildItem -Path $files_folders -Filter $search_criteria |
+    Where-Object {!$_.PSIsContainer} |
     Sort-Object { [regex]::Replace($_.Name, '\d+', { $args[0].Value.PadLeft(20) }) }
 )
 
 if ($foundFiles.Length -lt 2) {
-    Write-Output "No enough files to generate the command"
+    Write-Output "Not enough files to generate the command"
 } else {
-# First item of the array must be invoked by --load, instead of --append
-    $first_file = $foundFiles[0].Name
-    $other_files = $foundFiles | Select-Object -Skip 1
-    $appendedFiles = @($other_files | ForEach-Object {"--append " + $_.Name}) -join " "
-    $command =  '"' + $avidemux_cli + '"' + " --load " + $first_file + " " + $appendedFiles + " " + $avidemux_common_args
-    Write-Output $command
+    Set-Location $files_folders
+
+    # Prepare arguments for Avidemux
+    $argsList = @("--load", $foundFiles[0].Name)
+
+    foreach ($file in $foundFiles | Select-Object -Skip 1) {
+        $argsList += @("--append", $file.Name)
+    }
+
+    $argsList += $avidemux_common_args
+
+    # Run Avidemux CLI with the correct arguments
+    & "$avidemux_cli" @argsList
 }
