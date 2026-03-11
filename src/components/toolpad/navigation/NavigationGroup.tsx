397d001a374d4e9e1894278bbf7875879c3aa72e
jy95 <jacques.yakoub@gmail.com>
2026-03-11 20:27:09 +0100

    refactor: drop mui/toolpad (WIP)

--- src/components/toolpad/navigation/NavigationGroup.tsx
+++ src/components/toolpad/navigation/NavigationGroup.tsx
@@ -1,13 +1,12 @@
 "use client";
 
 import { useState } from "react";
-import { usePathname } from "next/navigation";
+import { usePathname } from "@/i18n/routing"; 
 import { Box, Collapse, List } from "@mui/material";
 import ExpandLess from "@mui/icons-material/ExpandLess";
 import ExpandMore from "@mui/icons-material/ExpandMore";
 import NavigationItem from "./NavigationItem";
 
-// Types
 import type { NavigationItem as Item } from "../types";
 
 function hasChildren(item: Item): item is Item & { children: Item[] } {
@@ -18,9 +17,11 @@ export default function NavigationGroup({ item }: { item: Item }) {
   const pathname = usePathname();
   const [open, setOpen] = useState(false);
   
-  const key = item.segment ?? item.title;
   const hasAnyChild = hasChildren(item);
   const parentPath = `/${item.segment}`;
+  
+  // La comparaison devient nativement correcte car pathname et parentPath 
+  // utilisent la même logique de résolution fournie par next-intl
   const isParentSelected = pathname === parentPath;
 
   return (
@@ -39,6 +40,7 @@ export default function NavigationGroup({ item }: { item: Item }) {
           <List component="div" disablePadding sx={{ mt: 0.5 }}>
             {item.children.map((child: any) => {
               const childPath = child.segment ? `${parentPath}/${child.segment}` : parentPath;
+              
               return (
                 <NavigationItem
                   key={child.segment ?? child.title}
