6e79991299605cddb5c2b9e13991b2f4c6195751
jy95 <jacques.yakoub@gmail.com>
2026-04-09 00:46:47 +0200

    chore: alignItems

--- src/components/toolpad/Branding.tsx
+++ src/components/toolpad/Branding.tsx
@@ -6,7 +6,7 @@ import { Box, Typography, Link as MuiLink } from '@mui/material';
 
 export default function Branding() {
     return (
-        <Box display="flex" alignItems="center" gap={1}>
+        <Box sx={{ alignItems: 'center', gap: 1, display: 'flex' }}>
             <Typography variant="h6" component="div">
                 <MuiLink component={Link} href="/" underline="none">
                     GamesPassionFR
