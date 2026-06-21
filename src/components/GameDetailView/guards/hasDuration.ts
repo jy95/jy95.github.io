36ef0342a39b39b1b408063827116da26944c04e
jy95 <jacques.yakoub@gmail.com>
2026-06-21 09:21:17 +0200

    refactor: redesign card

--- src/components/GameDetailView/guards/hasDuration.ts
+++ src/components/GameDetailView/guards/hasDuration.ts
@@ -1,6 +1,6 @@
 import hasKey from "./hasKey";
 import type { GameDetailsEntry } from "../types";
 
-export default function hasDuration(game: GameDetailsEntry): game is GameDetailsEntry & { duration: string } {
+export function hasDuration(game: GameDetailsEntry): game is GameDetailsEntry & { duration: string } {
     return hasKey(game, "duration", (val): val is string => typeof val === "string" && val !== "00:00:00");
 }
