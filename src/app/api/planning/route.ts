73a05853becb882ed5ba73fad4ac8f63f196c0ff
Jacques Yakoub <jacques.yakoub@gmail.com>
2026-08-10 23:38:43 +0200

    Refactor planningEntry type and enhanceGameItem function

--- src/app/api/planning/route.ts
+++ src/app/api/planning/route.ts
@@ -1,16 +1,17 @@
 import { NextResponse } from "next/server";
-import { buildCardEntry } from "@/redux/sharedDefintion";
-import type { BasicGame, CardEntry } from "@/redux/sharedDefintion";
+import { buildCardEntry } from "`@/redux/sharedDefintion`";
+import type { BasicGame, CardGame } from "`@/redux/sharedDefintion`";
 
 type rawEntry = Omit<BasicGame, "id">;
-export type planningEntry = Omit<BasicGame, "videoId" | "playlistId"> & {
+
+export type planningEntry = CardGame & {
     /** @description Still in progress or finished ? */
     status: "RECORDED" | "PENDING";
     /** @description When to display the game public, such as 20210412 (12/04/2021) */
     startAt?: number;
     /** @description When to display the game public, such as 20210420 (20/04/2021) */
     finishAt?: number;
-} & CardEntry;
+};
 
 export async function GET() {
     const games = (await import("./planning.json")).default;
@@ -22,23 +23,10 @@ export async function GET() {
     });
 }
 
-// Return an enhanced payload for a single game
 function enhanceGameItem(game: rawEntry): planningEntry {
-    const { id, url, url_type, imagePath, sourceKind } = buildCardEntry(game, "/covers", "planning");
-
     return {
-        id,
-        title: game.title,
-        platform: game.platform,
-        status: (game.hasOwnProperty("endAt") ? "RECORDED" : "PENDING"),
-        imagePath,
-        availableAt: game.availableAt,
-        endAt: game.endAt,
-        releaseDate: game.releaseDate,
-        duration: game.duration,
-        genres: game.genres,
-        url,
-        url_type,
-        sourceKind
-    }
+        ...game,
+        ...buildCardEntry(game, "/covers", "planning"),
+        status: Object.hasOwn(game, "endAt") ? "RECORDED" : "PENDING"
+    };
 }
