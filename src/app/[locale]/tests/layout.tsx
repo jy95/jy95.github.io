06d2f649ebda8762f2462f300fc3f519efbfe35e
jy95 <jacques.yakoub@gmail.com>
2026-08-23 20:39:03 +0200

    refactor: LocaleLayout

--- src/app/[locale]/tests/layout.tsx
+++ src/app/[locale]/tests/layout.tsx
@@ -1,17 +1,1 @@
-import {NextIntlClientProvider} from 'next-intl';
-
-// Types
-import type { ReactNode } from "react";
-
-type Props = {
-    children: ReactNode
-}
-
-export default async function StatsLaytou({children} : Props) {
-
-    return (
-        <NextIntlClientProvider>
-            {children}
-        </NextIntlClientProvider>
-    );
-}
+export { default } from "@/components/common/LocaleLayout";
