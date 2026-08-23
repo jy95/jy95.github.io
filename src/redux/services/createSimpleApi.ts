e432877858ac9b2de4825047e02cda06cd0f2422
jy95 <jacques.yakoub@gmail.com>
2026-08-23 21:32:15 +0200

    refactor: createSimpleGetApi

--- src/redux/services/createSimpleApi.ts
+++ src/redux/services/createSimpleApi.ts
@@ -2,18 +2,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export function createSimpleGetApi<
     TResponse,
-    ReducerPath extends string,
-    EndpointName extends string
 >(
-    reducerPath: ReducerPath,
+    reducerPath: string,
     path: string,
-    endpointName: EndpointName
+    endpointName: string
 ) {
     return createApi({
         reducerPath,
         baseQuery: fetchBaseQuery({ baseUrl: '/api' }),
         endpoints: (builder) => ({
             [endpointName]: builder.query<TResponse, void>({ query: () => path })
-        } as Record<EndpointName, ReturnType<typeof builder.query<TResponse, void>>>)
+        })
     });
 }
