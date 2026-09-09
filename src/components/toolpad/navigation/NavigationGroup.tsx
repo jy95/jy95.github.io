ff7f2811cfd2ca2930500f2e0d1eb61256e7410c
Jacques Yakoub <jacques.yakoub@gmail.com>
2026-09-09 19:59:19 +0200

    Remove unused styled import from NavigationGroup

--- src/components/toolpad/navigation/NavigationGroup.tsx
+++ src/components/toolpad/navigation/NavigationGroup.tsx
@@ -10,7 +10,6 @@ import ListItem from "@mui/material/ListItem";
 import ListItemButton from "@mui/material/ListItemButton";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import { styled } from "@mui/material/styles";
 import NavigationItem from "./NavigationItem";
 import { useAppContext } from "../provider/useAppContext";
 import type { NavigationItem as Item } from "../types";
