fcc30d0585aaa4db562a7c146e287c04872a98d8
jy95 <jacques.yakoub@gmail.com>
2026-09-12 00:20:19 +0200

    refactor: remove unused imports

--- src/app/[locale]/games/dlcs/page.tsx
+++ src/app/[locale]/games/dlcs/page.tsx
@@ -1,26 +1,12 @@
 "use client";
 
-// Next js
-import dynamic from 'next/dynamic'
-
 // Hooks
 import { useGetDLCsQuery } from "@/redux/services/dlcsAPI";
-
-// MUI component
-import Grid from '@mui/material/Grid';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 // Components
 import QueryErrorState from '@/components/common/QueryErrorState';
 import SkeletonGrid from '@/components/common/SkeletonGrid';
 import { GroupedGamesAccordion } from '@/features/games/components/GroupedGamesAccordion';
-
-// Custom
-const CardEntry = dynamic(() => import('@/features/games/components/CardEntry'), { ssr: false });
-const AccordionDetails = dynamic(() => import('@mui/material/AccordionDetails'), { ssr: false });
 
 // The gallery component
 function GamesGalleryList() {
