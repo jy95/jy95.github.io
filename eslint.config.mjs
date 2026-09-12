a3cd6b711d9fc42935c8962bd11a1e7efc6e5a66
jy95 <jacques.yakoub@gmail.com>
2026-09-12 18:50:22 +0200

    chore: fix typo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,5 @@
 import nextCoreWebVitals from "eslint-config-next/core-web-vitals";
 import nextTypescript from "eslint-config-next/typescript";
-import { dirname } from 'node:path'
-import { fileURLToPath } from 'node:url''
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
 
 const eslintConfig = [...nextCoreWebVitals, ...nextTypescript, {
   ignores: [
