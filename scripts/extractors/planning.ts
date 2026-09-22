291baacf0249a84e735ebda89a43d2bd2982dcb9
jy95 <jacques.yakoub@gmail.com>
2026-09-23 00:34:01 +0200

    refactor: simplify extractor query

--- scripts/extractors/planning.ts
+++ scripts/extractors/planning.ts
@@ -7,18 +7,7 @@ import type { Database } from "better-sqlite3";
  * Extracts planning from the database and saves them to a file.
  */
 export async function extractAndSavePlanning(db: Database, outputPath: string): Promise<void> {
-    const extractPlanningStmt = db.prepare(`
-        SELECT 
-            gif.*, 
-            g.*,
-            COALESCE(
-                (SELECT json_group_array(genre) 
-                 FROM games_genres 
-                 WHERE game = g.id), 
-            '[]') as genres
-        FROM games_in_future gif 
-        INNER JOIN games g ON g.id = gif.id
-    `);
+    const extractPlanningStmt = db.prepare(`SELECT * FROM games_in_future`);
     const planning = extractPlanningStmt.all();
     await writeFile(
         outputPath,
